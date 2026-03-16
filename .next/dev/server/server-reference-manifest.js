0d39a04994e1142be38167a9780d4f09d6ee2ef5
Sooraj S Nair <soorajnair257@gmail.com>
2026-03-16 12:12:10 +0530

    remove next build folder properly

--- .next/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"TkQHD/r01Xzv2WaQSf+XO3csoNpGVv2nPvgkjYZzrkc=\"\n}"
--- .next/dev/static/chunks/app_page_tsx_893e5ca7._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules_a25ce1ef._.js",
-  "static/chunks/_05f4e343._.js"
-],
-    source: "dynamic"
-});
